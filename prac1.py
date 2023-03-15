fd654749cca38f3640d614a5f17384b8f662323c
cauyoon <127828653+cauyoon@users.noreply.github.com>
2023-03-15 13:19:08 +0900

    Create prac1.py

---
+++ prac1.py
@@ -0,0 +1,10 @@
+width = input("가로:")
+length = input("세로:")
+height = input("높이:")
+
+volume = float(width) * float(length) * float(height)
+statement1 = str("박스의 부피는 ")
+statement2 = str("입니다.")
+
+final_statemnet = str(statement1) + str(volume) + str(statement2)
+print(final_statemnet)
