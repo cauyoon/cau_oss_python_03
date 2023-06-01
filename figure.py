53667956c04c758541bbd776ac8a1e1d7cc1d204
cauyoon <williamha13@gmail.com>
2023-06-01 17:13:20 +0900

    implement hw5

--- figure.py
+++ figure.py
@@ -1,20 +1,26 @@
 import math
 class line:
-    __length = 0
-    def __init__(self, length):
-        self.__length = length
+    __width = 0
+    __height = 0
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
     
-    def set_length(self, length):
-        self.length = length
+    def set_length(self, width, height):
+        self.width = width
+        self.height = height
 
     def get_length(self):
-        return self.__length
+        return self.__width, self.__height
     
-    def area_square(length):
-        return length*length
+    def area_rectangle(width, height):
+        if width <= 0 or height <= 0: raise ValueError
+        return width * height
     
-    def area_circle(length):
-        return length*length*math.pi
+    def area_ellipse(width, height):
+        if width <= 0 or height <= 0: raise ValueError
+        return width * height * math.pi
     
-    def area_regular_triangle(length):
-        return length*length*math.sqrt(3)/4
+    def area_right_triangle(width, height):
+        if width <= 0 or height <= 0: raise ValueError
+        return width * height / 2
